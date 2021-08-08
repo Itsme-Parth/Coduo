f0205ccb52a1e81ae89d95df99b9fbe6148dc234
Parth Chadha <parthc2001@gmail.com>
2021-08-08 17:29:23 +0530

    Edit: App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
 function App() {
+  const [code, setCode] = useState(""); //code is the variable that contains the text of the textarea and setCode sets the text to the textarea
+  const handleSubmit = () => {
+    console.log(code);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Online Code compiler</h1>
+      <textarea
+        rows="20"
+        cols="70"
+        value={code}
+        onChange={(e) => {
+          setCode(e.target.value);
+        }}
+      ></textarea>
+      <br />
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
