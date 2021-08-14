921b3cc32289dc49c7e0d90d71b87149795d3249
Ojaswi Singh <singh.ojaswi1013@gmail.com>
2021-08-15 01:58:07 +0530

    EDIT: helpers ADD c and java

--- backend/helpers.js
+++ backend/helpers.js
@@ -8,12 +8,28 @@ const { resolve } = require("path");
 
 const codeDir = path.join(__dirname, "temp");
 
+//Renaming Main class as the filename
+exports.updateName = async (code, filename) => {
+  try {
+    return await code.replace(/Main/g, `${filename.split(`.`)[0]}`);
+  } catch (err) {
+    return err;
+  }
+};
+
 // For cerating and initializing the program file
 exports.createfile = async (format, code) => {
-  const filename = `${uuidv4()}.${format}`;
+  const uuidgen = `_${uuidv4()}`;
+  const filename = await `${uuidgen.replace(/-/g, `_`)}.${format}`;
   const filepath = path.join(codeDir, filename);
-  await fs.writeFileSync(filepath, code);
-  return filepath;
+  try {
+    if (format === "java") code = await this.updateName(code, filename);
+    await fs.writeFileSync(filepath, code);
+    return filepath;
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, error: "Something wenr wrong!" });
+  }
 };
 
 // For executing the c++ program file and returning Input
@@ -47,6 +63,47 @@ exports.executepy = async (filepath) => {
         stderr && reject(stderr); // if (stderr) reject(stderr);
         resolve(stdout);
       });
+    });
+  } catch (err) {
+    console.log(`Error here: ${err}`);
+    return;
+  }
+};
+
+// For executing the c program file and returning Input
+exports.executec = async (filepath) => {
+  try {
+    const filename = path.basename(filepath).split(".")[0];
+    const outfile = path.join(codeDir, `${filename}.out`);
+    return new Promise((resolve, reject) => {
+      exec(
+        `gcc ${filepath} -o ${outfile} && cd temp && ${filename}.out`,
+        (error, stdout, stderr) => {
+          error && reject({ error, stderr }); // if (error) reject(error);
+          stderr && reject(stderr); // if (stderr) reject(stderr);
+          resolve(stdout);
+        }
+      );
+    });
+  } catch (err) {
+    console.log(`Error here: ${err}`);
+    return;
+  }
+};
+
+// For executing the java program file and returning Input
+exports.executejava = async (filepath) => {
+  try {
+    const filename = path.basename(filepath).split(".")[0];
+    return new Promise((resolve, reject) => {
+      exec(
+        `javac ${filepath} && cd temp && java ${filename}`,
+        (error, stdout, stderr) => {
+          error && reject({ error, stderr }); // if (error) reject(error);
+          stderr && reject(stderr); // if (stderr) reject(stderr);
+          resolve(stdout);
+        }
+      );
     });
   } catch (err) {
     console.log(`Error here: ${err}`);
